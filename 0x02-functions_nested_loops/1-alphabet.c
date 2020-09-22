4940cccf343350723e2fd1671068b43ef2e2b005
Jong9106 <2269@holbertonschool.com>
2020-09-22 04:07:48 +0000

    loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char c;
 
-	for(c = 'a' ; c <= 'z'; c++);
-	_putchar(a);
+	for(c = 'a' ; c <= 'z'; c++)
+	{
+	_putchar(c);
 	}
-_putchar('\n');
+	_putchar('\n');
 }
