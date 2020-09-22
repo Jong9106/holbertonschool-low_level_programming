bda442d5c82dcc1f43124ddb47efcb639d7e01db
Jong9106 <2269@holbertonschool.com>
2020-09-22 03:59:17 +0000

    loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for(c = 'a' ; c <= 'z'; c++);
+	_putchar(a);
+	}
+_putchar('\n');
+}
