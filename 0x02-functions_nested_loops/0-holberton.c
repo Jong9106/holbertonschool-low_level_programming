11f16f26da5c410fe486eae3d0979e95a8476618
Jong9106 <2269@holbertonschool.com>
2020-09-22 17:24:58 +0000

    loop

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,11 +9,11 @@ int main(void)
 	_putchar('H');
 	_putchar('o');
 	_putchar('l');
-        _putchar('b');
+	_putchar('b');
 	_putchar('e');
-        _putchar('r');
+	_putchar('r');
 	_putchar('t');
-        _putchar('o');
+	_putchar('o');
 	_putchar('n');
 	_putchar('\n');
 return (0);
