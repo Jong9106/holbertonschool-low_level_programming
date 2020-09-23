dbb3102fbcd05d0f3c7b14def5a7daa15b903827
Jong9106 <2269@holbertonschool.com>
2020-09-23 21:03:37 +0000

    loop

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,16 +7,22 @@
  */
 int main(void)
 {
-	long int i, n = 48, t1 = 1, t2 = 2, nextTerm;
+	long int fibonacci, n, j, k;
 
-	for (i = 2; i <= n; i++)
+	n = 0;
+	j = 1;
+	for (fibonacci = 0; fibonacci < 50; fibonacci++)
 	{
-	printf("%ld, ", t1);
-	nextTerm = t1 + t2;
-	t1 = t2;
-	t2 = nextTerm;
-	if (t1 == 4807526976)
-	putchar('\n');
+		k = n + j;
+		n = j;
+		j = k;
+		printf("%ld", k);
+		if (fibonacci == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
 	}
 return (0);
 }
