bb2ee267558881fb7e3a0f972c909e9950ebba22
Jong9106 <2269@holbertonschool.com>
2020-09-23 20:54:20 +0000

    loop

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	long int i, n = 50, t1 = 1, t2 = 2, nextTerm;
+	long int i, n = 48, t1 = 1, t2 = 2, nextTerm;
 
 	for (i = 2; i <= n; i++)
 	{
@@ -15,7 +15,8 @@ int main(void)
 	nextTerm = t1 + t2;
 	t1 = t2;
 	t2 = nextTerm;
+	if (t1 == 4807526976)
+	putchar('\n');
 	}
-putchar('\n');
 return (0);
 }
