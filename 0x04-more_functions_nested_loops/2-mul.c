3cbb78607964a9fe71ee6737be1f23b1f0ab7ab1
Jong9106 <2269@holbertonschool.com>
2020-09-24 22:56:06 +0000

    nest

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Entry point
+ * @a:integres tu mult
+ * @b: integrer tu mul
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
