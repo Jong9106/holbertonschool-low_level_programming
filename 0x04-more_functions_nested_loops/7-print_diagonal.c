30c21aaf85ce47a963fc1632cfba866ba48b9c78
Jong9106 <2269@holbertonschool.com>
2020-09-25 04:09:55 +0000

    nest

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: number
+ * espacio: count
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int lineas, espacio;
+
+	if (n > 0)
+	{
+		for (lineas = 0 ; lineas < n ; lineas++)
+		{
+			for (espacio = 0 ; espacio < lineas ; espacio++)
+			{
+			_putchar(32);
+			}
+		_putchar(92);
+		_putchar(10);
+		}
+	}
+
+	else
+	{
+	_putchar(10);
+	}
+
+}
