f5771e5effc41e81c40e795d9b1d56851c45ad8d
Jong9106 <2269@holbertonschool.com>
2020-09-25 17:07:21 +0000

    nest

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,27 +11,17 @@ int main(void)
 
 	for (numbers = 1 ; numbers <= 100 ; ++numbers)
 	{
-		if (numbers % 3 == 0 && numbers % 5 == 0)
-		{
-		printf("FizzBuzz");
-		putchar(32);
-		continue;
-		}
-		if (numbers % 3 == 0)
-		{
-		printf("Fizz");
-		putchar(32);
-		continue;
-		}
-		if (numbers % 5 == 0)
-		{
-		printf("Buzz");
-			if (numbers == 100)
-			putchar(10);
-		putchar(32);
-		continue;
-		}
-	printf("%d ", numbers);
+		if (numbers % 15 == 0)
+			printf("FizzBuzz");
+		else if (numbers % 3 == 0)
+			printf("Fizz");
+		else if (numbers % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", numbers);
+
+		if (numbers != 100)
+			printf(" ");
 	}
 putchar(10);
 return (0);
