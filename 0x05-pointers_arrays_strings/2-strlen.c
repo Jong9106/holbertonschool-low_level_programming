0a5baac7e889d44b0b5eae91d573cb2782ba9fde
Jong9106 <2269@holbertonschool.com>
2020-09-29 17:58:21 +0000

    array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Entry point
+ * @s: one
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	l++;
+	return(l);
+}
