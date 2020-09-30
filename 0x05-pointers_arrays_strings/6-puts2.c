7fa927e6e728ea49f4344a63d4ae396e15447dee
Jong9106 <2269@holbertonschool.com>
2020-09-30 03:27:01 +0000

    half lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Entry point
+ * @str: one
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+	int contador;
+
+	for (contador = 0 ; str[contador] != '\0' ; contador++)
+	{
+	if (str[contador] % 2 == 0)
+	_putchar(str[contador]);
+	}
+_putchar(10);
+}
