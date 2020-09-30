bf523825dadefaf7d5dbbbba03747338de3e55be
Jong9106 <2269@holbertonschool.com>
2020-09-30 04:18:44 +0000

    array

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts_half - Entry point
+ * @str: one
+ * Return: Always 0 (Success)
+ */
+void puts_half(char *str)
+{
+	int contador, par, impar;
+
+	for (contador = 0 ; str[contador] != '\0' ; contador++)
+	{
+	}
+	if (contador % 2 == 0)
+		{
+		for (par = contador / 2 ; par < contador ; par++)
+		_putchar(str[par]);
+		}
+	else
+		{
+		for (impar = ((contador - 1) / 2) ; impar < contador ; impar++)
+		_putchar(str[impar]);
+		}
+_putchar(10);
+}
