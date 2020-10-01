c5a7f1ca7f6e437ef853bbb3eb29d9129333dd2f
Jong9106 <2269@holbertonschool.com>
2020-10-01 04:52:29 +0000

    pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - Entry point
+ * @dest: pointer
+ * @src: entrada
+ * Return: Always 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int contador;
+
+	for (contador = 0 ; src[contador] != '\0' ; contador++)
+	{
+		dest[contador] = src[contador];
+	}
+return (dest);
+}
