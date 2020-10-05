a9f5b76024868bd3d26461dd4c2438cc43008e09
Jong <juank_hr@hotmail.com>
2020-10-05 23:25:45 +0000

    Pointer to a pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,22 @@
-
 #include "holberton.h"
 
 /**
- * *rot13 - Entry point
- * @s: pointer
+ * *_memset - Entry point
+ * @s: array
+ * @b: value
+ * @n: byte const
  * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (; n > 0 ; n--, s++)
-	*s = b;
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
 return (s);
 }
