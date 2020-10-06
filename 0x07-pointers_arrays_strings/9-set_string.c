adb421989c5c0755dfe96069fe37ef2692a5bd6c
Jong <juank_hr@hotmail.com>
2020-10-06 05:16:03 +0000

    Pointer to a pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - Entry point
+ * @s: pointer to pointer
+ * @to: char
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
