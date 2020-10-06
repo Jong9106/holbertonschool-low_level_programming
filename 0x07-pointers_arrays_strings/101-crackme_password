6667cf7858551983fccc751e9326bf92baf53e13
Jong <juank_hr@hotmail.com>
2020-10-06 04:49:39 +0000

    Pointer to a pointers

---
+++ 0x07-pointers_arrays_strings/101-crackme_password
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - Entry point
+ * @s: string
+ * @c: compare
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	for (; ; s++)
+	{
+		if (*s == c)
+		{
+		return (s);
+		}
+		else if (*s == '\0')
+		{
+		return ('\0');
+		}
+	}
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy - Entry point
+ * @dest: destino
+ * @src: fuente
+ * @n: byte const
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	for (; ; s++)
+	{
+		for (; ; accept++)
+		if (*s == c)
+		{
+		return (s);
+		}
+		else if (*s == '\0')
+		{
+		return ('\0');
+		}
+	}
+}
