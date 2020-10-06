2eec7f6a1713628190d93ed0509e342850b18d76
Jong <juank_hr@hotmail.com>
2020-10-06 04:59:47 +0000

    Pointer to a pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - Entry point
+ * @s: string
+ * @accept: second string
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0 ; s[i] != '\0' ; s++)
+	{
+		for (j = 0 ; accept[j] != '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+			return (s);
+			}
+			else if (s[i] == '\0')
+			{
+			return ('\0');
+			}
+		}
+	}
+return ('\0');
+}
