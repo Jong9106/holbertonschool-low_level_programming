506e16e3a236150dcf022c5b535ab591630b55ab
Jong <juank_hr@hotmail.com>
2020-10-07 03:26:06 +0000

    Pointer to a pointer

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,26 +9,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int j, i;
+	int j, i, start;
 
-	for (i = 0 ; haystack ; i++)
+	i = 0;
+	while (haystack[i] != '\0')
 	{
+		start = i;
 		j = 0;
-		while (needle[j] != '\0' && haystack[i] == needle[j])
+		while (haystack[i] == needle[j] &&
+			needle[j] != '\0' && haystack[i] != '\0')
 		{
-			j++;
 			i++;
+			j++;
 		}
 
-		if (j > 0)
-		{
-			i -= j;
-			if (needle[j] == '\0')
-			{
-				return (haystack + i);
-			}
-		break;
-		}
+		if (needle[j] == '\0')
+			return (haystack + start);
+		i = start + 1;
 	}
 return ('\0');
 }
