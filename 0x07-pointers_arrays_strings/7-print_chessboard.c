f07d0b2aad6ff8349a0d9b965aae16b2cd4fe8f7
Jong <juank_hr@hotmail.com>
2020-10-07 04:29:08 +0000

    Pointer to a pointer

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - Entry point
+ * @haystack: string
+ * @needle: second string
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+}
