8f6a2b0beb39cf3479282055821491c4cf6109bb
root <root@Jong-PC.localdomain>
2020-10-07 19:26:59 -0500

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,20 @@
 #include "holberton.h"
 
 /**
- * *_memcpy - Entry point
- * @dest: destino
- * @src: fuente
- * @n: byte const
+ * _puts_recursion - Entry point
+ * @s: poiter to a string
  * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
