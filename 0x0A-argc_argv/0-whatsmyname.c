0bb5ed2458b56c39ce68f42f373abbaffe090913
Jong9106 <juank_hr@hotmail.com>
2020-10-09 17:29:38 -0500

    My name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @i: poiter to a string
+ * Return: Always 0 (Success)
+ */
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
