68b2ec22d4610285d2e0cb1d98e36e7b757b4c79
Jong9106 <juank_hr@hotmail.com>
2020-10-19 18:30:51 -0500

    Structure for 0-main.c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef __DOG_H_
+#define __DOG_H_
+
+/**
+ * struct dog - structure for 0-main.c
+ * @name: pointer to name
+ * @age: age number
+ * @owner: pointer to owner
+ *
+ * Description: structure to define dogs documentation
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
