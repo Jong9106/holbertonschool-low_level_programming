c66b80bdee8fd1bcd34bee5a84e1799e8695671f
Jong9106 <juank_hr@hotmail.com>
2020-10-19 22:22:00 -0500

    Add a new prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,10 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+int _putchar(char c);
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #endif
