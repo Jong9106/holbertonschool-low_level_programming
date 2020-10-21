ae11492ec4740b8589153097095d77ae0643d8d2
Jong9106 <juank_hr@hotmail.com>
2020-10-21 15:30:02 -0500

    Delete commits

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,13 +5,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-//* Function that prints a name /*
+
 void print_name(char *name, void (*f)(char *));
 
-//* Function that executes a function given as a parameter on each element of an array /*
 void array_iterator(int *array, size_t size, void (*action)(int));
 
-//* Function that searches for an integer /*
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
