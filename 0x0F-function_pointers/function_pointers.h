d8226ab4b23d2beb2d5439d71c6747937473f575
Jong9106 <juank_hr@hotmail.com>
2020-10-21 13:45:48 -0500

    Prototype library

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,17 @@
+#ifndef _F_H_
+#define _F_H_
+
+#include <stidio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+//* Function that prints a name /*
+void print_name(char *name, void (*f)(char *));
+
+//* Function that executes a function given as a parameter on each element of an array /*
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+//* Function that searches for an integer /*
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
