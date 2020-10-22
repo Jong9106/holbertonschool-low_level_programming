31449fba3b305ea7a787f2a7c19c571629e94ed7
Jong9106 <juank_hr@hotmail.com>
2020-10-22 11:21:17 -0500

    Delete 99 error and modified for betty style

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,6 +2,8 @@
 
 /**
  *main - entry point
+ *@argc: number of arguments
+ *@argv: arguments in pront
  *
  *Return: 0 always (success)
  */
@@ -9,29 +11,22 @@ int main(int argc, char *argv[])
 {
 	int calc, num1, num2;
 
-	int (*ptr)(int,int);
+	int (*ptr)(int, int);
+
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	ptr = get_op_func(argv[2]);
 
-	if(argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[3]);
-ptr = get_op_func(argv[2]);
 
-	if(ptr == NULL)
-	{
-		printf("Error");
-		exit(99);
-	}
+	calc = ptr(num1, num2);
 
-	calc = ptr(num1, num2)
-
-	printf("%d\n",calc);
+	printf("%d\n", calc);
 	return (0);
 }
