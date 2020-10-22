36a6708aa03fa75b901a351a5f6394f2ab842dbd
Jong9106 <juank_hr@hotmail.com>
2020-10-22 11:15:26 -0500

    fixed Betty style

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,10 @@
 #ifndef __CALC_H__
 #define __CALC_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 /**
  * struct op - Struct op
  *
@@ -9,8 +13,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
