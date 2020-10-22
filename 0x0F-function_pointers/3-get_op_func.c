a1ef6a3c4cff32c1a913838af4596e49951ce521
Jong9106 <juank_hr@hotmail.com>
2020-10-22 11:19:49 -0500

    Change Return and conditional

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,21 +9,27 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	char sign;
-
-	while (sign != s
-	{
-		return (NULL);
-	}
-
-	if
-	{
-		op_t ops[] ={
-        	{"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
-        	{NULL, NULL}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 		};
-    int i;
+
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (*ops[i].op == *s)
+		{
+			return (ops[i].f);
+		}
+
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
