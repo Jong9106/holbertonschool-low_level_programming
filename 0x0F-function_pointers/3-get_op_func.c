7b80b0b5b75782a7b0a493b25eaec2729cba4c5d
Jong9106 <juank_hr@hotmail.com>
2020-10-21 23:46:59 -0500

    Function pointer to op

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ *get_op_func - pointer function to op
+ *@s: ponter to function
+ *
+ *Return: 0
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	char sign;
+
+	while (sign != s
+	{
+		return (NULL);
+	}
+
+	if
+	{
+		op_t ops[] ={
+        	{"+", op_add},
+        	{"-", op_sub},
+        	{"*", op_mul},
+        	{"/", op_div},
+        	{"%", op_mod},
+        	{NULL, NULL}
+		};
+    int i;
