982882eeddb126dd336a227738b5eb6c9191cffd
Jong9106 <juank_hr@hotmail.com>
2020-11-04 15:05:53 -0500

    Function to free memory for a list and set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function to free memory
+ * @head: Popinter to struct
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	free(*head);
+}
