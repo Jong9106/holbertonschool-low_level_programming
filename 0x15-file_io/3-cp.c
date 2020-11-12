e11c3da2e4ea313ca460d4a48c576080b7dbf521
Jong9106 <juank_hr@hotmail.com>
2020-11-12 12:41:12 -0500

    Correct value of close

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char **argv)
 {
-	int file_from, file_to, f_read, f_write, cl;
+	int file_from, file_to, f_read, f_write;
 	char buffer[BUFFER];
 
 	if (argc != 3)
@@ -17,9 +17,9 @@ int main(int argc, char **argv)
 	if (!argv[1])
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
-	if (argv[2] == NULL)
-		dprintf(2, "Error: Can't write to %s\n", argv[2]),
-		exit(99);
+	if (!argv[2])
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]),
+		exit(98);
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	f_read = read(file_from, buffer, BUFFER);
@@ -37,11 +37,11 @@ int main(int argc, char **argv)
 			dprintf(2, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
 	}
-	cl = close(file_from);
-	if (cl == -1)
-		dprintf(2, "Can't close fd %d\n", file_from), exit(100);
-	cl = close(file_to);
-	if (cl == -1)
-		dprintf(2, "Can't close fd %d\n", file_to), exit(100);
+	f_write = close(file_from);
+	if (f_write == -1)
+		dprintf(2, "Error: Can't close fd %d\n", file_from), exit(100);
+	f_write = close(file_to);
+	if (f_write == -1)
+		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
