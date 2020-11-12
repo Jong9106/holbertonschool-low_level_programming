2c85c6ce57b634b519cbcbfc1b8dab06ccfc5e35
Jong9106 <juank_hr@hotmail.com>
2020-11-12 00:53:57 -0500

    Change malloc from array

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,30 +9,30 @@
 int main(int argc, char **argv)
 {
 	int file_from, file_to, f_read, f_write, cl;
-	char *mem;
+	char buffer[BUFFER];
 
 	if (argc != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"),
 		exit(97);
-	file_from = open(argv[1], O_RDONLY);
-	mem = malloc(sizeof(char) * BUFFER);
-	if (!mem)
+	if (!argv[1])
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
+	if (!argv[2])
 		dprintf(2, "Error: Can't write to %s\n", argv[2]),
 		exit(99);
-	f_read = read(file_from, mem, BUFFER);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	f_read = read(file_from, buffer, BUFFER);
 	if (f_read == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
 	while (f_read != 0)
 	{
-		f_write = write(file_to, mem, f_read);
+		f_write = write(file_to, buffer, f_read);
 		if (f_write == -1 || f_write != f_read)
 			dprintf(2, "Error: Can't write to %s\n", argv[2]),
 			exit(99);
-		f_read = read(file_from, mem, BUFFER);
+		f_read = read(file_from, buffer, BUFFER);
 		if (f_read == -1)
 			dprintf(2, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
@@ -43,6 +43,5 @@ int main(int argc, char **argv)
 	cl = close(file_to);
 	if (cl == -1)
 		dprintf(2, "Can't close fd %d\n", file_to), exit(100);
-	free(mem);
 	return (0);
 }
