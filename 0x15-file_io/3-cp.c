bc34b9e883e97b32b34eb6105194c7dbed063679
Jong9106 <juank_hr@hotmail.com>
2020-11-12 00:37:33 -0500

    Added argv 1 y 2 == NULL

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,18 +14,15 @@ int main(int argc, char **argv)
 	if (argc != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"),
 		exit(97);
-	if (!argv[1])
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]),
-		exit(98);
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	if (file_from == -1 || argv[1] == NULL)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
 	mem = malloc(sizeof(char) * BUFFER);
 	if (!mem)
 		exit(98);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
+	if (file_to == -1 || argv[2] == NULL)
 		dprintf(2, "Error: Can't write to %s\n", argv[2]),
 		exit(99);
 	f_read = read(file_from, mem, BUFFER);
