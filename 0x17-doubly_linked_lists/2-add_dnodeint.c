593b3b2068dc46e099d58d96c4e288038f582228
Jong9106 <juank_hr@hotmail.com>
2020-12-14 00:48:35 -0500

    Function to add a new node in head of a list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function to add a node
+ * @head: pointer to head pointer
+ * @n: data to add in new node
+ * Return: Data inside of all nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head)
+		(*head)->prev = new_node;
+	*head = new_node;
+	return (new_node);
+}
