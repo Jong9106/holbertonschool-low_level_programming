0b6905253130cc8e555e13a9189996bf3bc959c9
Jong9106 <juank_hr@hotmail.com>
2021-01-15 11:21:01 -0500

    change conditional

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,8 +36,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node->next = current_node;
 	}
 
-	ht->array[index] = new_node;
 	new_node->next = NULL;
+	ht->array[index] = new_node;
+	
 
 	return (1);
 
