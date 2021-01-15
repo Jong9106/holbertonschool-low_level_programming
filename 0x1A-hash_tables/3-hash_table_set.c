d3457eac853827c6b9a34281b592b94bc69cea1a
Jong9106 <juank_hr@hotmail.com>
2021-01-15 13:40:38 -0500

    delete condition for empty value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,13 +13,12 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *new_node;
-	hash_node_t *old_node;
+	hash_node_t *new_node, *old_node;
+
+	if (!ht  || !key || strlen(key) == 0)
+		return (0);
 
 	index = key_index(((unsigned char *)key), ht->size);
-
-	if (!ht  || !ht->array || !key || !value || strlen(key) == 0)
-		return (0);
 
 	if (ht->array[index])
 	{
