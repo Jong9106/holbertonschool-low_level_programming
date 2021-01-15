6b366838f35e8b6a5e0fe0a395e197deefa1379e
Jong9106 <juank_hr@hotmail.com>
2021-01-15 13:38:14 -0500

    Added new conditional and ask if index have already a node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,6 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * hash_table_set -  Function that adds an element to the hash table.
@@ -10,37 +12,42 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *new_key = NULL;
-	char *new_value = NULL;
 	unsigned long int index = 0;
-	hash_node_t *new_node = NULL;
-	hash_node_t *current_node = NULL;
+	hash_node_t *new_node;
+	hash_node_t *old_node;
 
-	index = key_index(((const unsigned char *)key), ht->size);
+	index = key_index(((unsigned char *)key), ht->size);
 
-	if (!ht || !key)
+	if (!ht  || !ht->array || !key || !value || strlen(key) == 0)
 		return (0);
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
-		return (0);
-
-	new_key = ((char *)key);
-	new_value = ((char *)value);
-	new_node->key = new_key;
-	new_node->value = new_value;
-	
-	ht->array[index] = new_node;
 
 	if (ht->array[index])
 	{
-		current_node = ht->array[index];
-		new_node->next = current_node;
+		old_node = ht->array[index];
+		while (old_node)
+		{
+			if (!(strcmp(old_node->key, key)))
+			{
+				free(old_node->value), old_node->value = strdup(value);
+				return (1);
+			}
+			old_node = old_node->next;
+		}
 	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 
-	new_node->next = NULL;
-
-
+	if (!new_node->key || !new_node->value)
+	{
+		if (new_node->key)
+			free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
-
 }
