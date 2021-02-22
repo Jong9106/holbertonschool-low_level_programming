a4dd04306e0a4a2f51b281a984332862a14f5b71
Jong9106 <juank_hr@hotmail.com>
2021-02-21 23:28:22 -0500

    ask for null positions

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,9 +8,19 @@ def island_perimeter(grid):
     """
     Function that return the iland perimetrer
     """
+    row = len(grid)
+    columns = len(grid[0])
     p = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
+    for i in range(row):
+        for j in range(columns):
+            if grid[i][j] == 0:
+                continue
+            if i == 0 or grid[i - 1][j] == 0:
                 p += 1
-    return p * 2 + 2
+            if i == row - 1 or grid[i + 1][j] == 0:
+                p += 1
+            if j == 0 or grid[i][j - 1] == 0:
+                p += 1
+            if j == columns - 1 or grid[i][j + 1] == 0:
+                p += 1
+    return p
