ef8ee686f9d932b3c24fd08f5d816da28cf5a50c
Jong9106 <juank_hr@hotmail.com>
2021-02-21 23:04:44 -0500

    multiply the variable

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,9 @@ def island_perimeter(grid):
     """
     Function that return the iland perimetrer
     """
-    p = 2
+    p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[j][i+1] == 1:
-                    p += 1
-                else:
-                    p += 2
-    return (p)
+                p += 1
+    return p * 2 + 2
