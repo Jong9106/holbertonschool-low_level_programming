93e86281e143005c37acd2dc34eba0e3672f20a9
Jong9106 <juank_hr@hotmail.com>
2021-04-19 21:08:12 -0500

    Function to search a value at index

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches for a value in an array
+ * @array: entry array
+ * @size: size of array
+ * @value: to search
+ * Return: position in array or -1 for error
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	(void) size;
+	unsigned int i = 0;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (; array[i]; i++)
+	{
+		if (i >= size)
+		{
+			return (-1);
+		}
+		if (array[i] == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			return (i);
+		}
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	}
+	return (0);
+}
