e3baa1d7da9f50298fc559a8308ee6c5ce52852e
Jong9106 <juank_hr@hotmail.com>
2021-04-20 23:39:01 -0500

    Serach in interpolation way

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - function that searches for a value in an array
+ * @array: entry array
+ * @size: size of array
+ * @value: to search
+ * Return: position in array or -1 for error
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	unsigned int low = 0, high = size - 1;
+	size_t pos = 0;
+
+	if (array)
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+					(value - array[low]));
+
+		if (array[pos] != value && pos < size)
+		{
+			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+			pos += 1;
+			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+			return (pos);
+		}
+		else if (array[pos] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+			return (pos);
+		}
+		else
+		{
+			printf("Value checked array[%ld] is out of range \n", pos);
+		}
+	}
+	return (-1);
+}
