596a147d3e36c6a0c0377831f3d3f8c1bea08ab8
Jong9106 <juank_hr@hotmail.com>
2021-04-21 00:52:51 -0500

    space deleted

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -21,7 +21,6 @@ int interpolation_search(int *array, size_t size, int value)
 		if (pos > size)
 		{
 			printf("Value checked array[%ld] is out of range\n", pos);
-			return (-1);
 		}
 		else if (array[pos] == value)
 		{
